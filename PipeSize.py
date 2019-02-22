7472103a8a42fa17ea4d9d9f545369a384d34c10
JarodAert <jarod.aerts@huskers.unl.edu>
2019-02-22 13:26:39 -0600

    Minor changes

--- PipeSize.py
+++ PipeSize.py
@@ -7,13 +7,8 @@ wallThickness=float(input("Enter wall thickness (in): "))
 pipeLength=float(input("Enter length of tube (m): "))
 
 pipeDiameter=pipeDiameter*2.54*10
-pipeRadius=pipeDiameter/2
 wallThickness=wallThickness*2.54*10
-#pipeLength=pipeLength*2.54
 
-print("Pipe Diameter (cm): "+str(pipeDiameter))
-print("Wall Thickness (cm): "+str(wallThickness))
-print("Pipe Length: "+str(pipeLength))
 
 crossSectionArea=math.pow(pipeDiameter,2) - math.pow(pipeDiameter-2*wallThickness,2)
 
