6165f861bcce580ff5e434a9c326a69f85849bf0
Jacob Sullivan <jacobsullivan@skylars-air.unl.edu>
2019-02-22 15:38:06 -0600

    added buoyant force calculation

--- PipeSize.py
+++ PipeSize.py
@@ -2,6 +2,9 @@ import math
 # Density of steel in g/cm3
 steelDensity=7.7
 
+# Density of water in g/cm3
+densityOfWater=1
+
 pipeDiameter=float(input("Enter Diameter of Pipe (in): "))
 wallThickness=float(input("Enter wall thickness (in): "))
 pipeLength=float(input("Enter length of tube (m): "))
@@ -11,7 +14,17 @@ wallThickness=wallThickness*2.54*10
 
 
 crossSectionArea=math.pow(pipeDiameter,2) - math.pow(pipeDiameter-2*wallThickness,2)
+volumeOfPipe = crossSectionArea * pipeLength * math.pi/4000
 
-mass=steelDensity * crossSectionArea * pipeLength * math.pi/4000
+mass=steelDensity * volumeOfPipe
+weightOfPipe = mass * 9.8
 
 print(str(mass)+" Kilograms")
+print(str(volumeOfPipe) +" volume of pipe")
+print(str(weightOfPipe)+" N = Weight of pipe")
+
+volumeOfWaterDisplaced = volumeOfPipe
+
+Fbuoyant = densityOfWater * 9.8 * volumeOfWaterDisplaced
+
+print(str(Fbuoyant)+" N Buoyant Force")
