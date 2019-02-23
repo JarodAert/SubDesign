e08de831d88b9216322684d1d87713ad705eafe8
Jacob Sullivan <jacobsullivan@skylars-air.unl.edu>
2019-02-22 18:56:11 -0600

    actually fixed it

--- PipeSize.py
+++ PipeSize.py
@@ -1,31 +1,45 @@
 import math
-# Density of steel in g/cm3
-steelDensity=7.7
 
-# Density of water in g/cm3
-densityOfWater=1
+# Density of steel in kg/m^3
+steelDensity = 7700
+# Density of water in kg/m^3
+densityOfWater = 1000
 
-pipeDiameter=float(input("Enter Diameter of Pipe (in): "))
-wallThickness=float(input("Enter wall thickness (in): "))
-pipeLength=float(input("Enter length of tube (m): "))
+pipeDiameter = float(input("Enter Diameter of Pipe (in): "))
+wallThickness = float(input("Enter wall thickness (in): "))
+pipeLength = float(input("Enter length of tube (m): "))
 
-pipeDiameter=pipeDiameter*2.54*10
-wallThickness=wallThickness*2.54*10
+# conversion from inches to m
+pipeDiameter = (pipeDiameter * 2.54) / 100
+wallThickness = (wallThickness * 2.54) / 100
 
+# find volume of just the metal part of the cylinder
+# volume of a cylinder = radius^2 * height * pi
+# volume in units of m^3
+volumeOfOutsidePipe = math.pow((pipeDiameter / 2), 2) * pipeLength * math.pi
+volumeOfInsidePipe = math.pow(((pipeDiameter - (wallThickness * 2)) / 2), 2) * pipeLength * math.pi
+volumeOfJustPipe = volumeOfOutsidePipe - volumeOfInsidePipe
+print(str(volumeOfOutsidePipe) + " m^3 = volume of outside pipe")
+print(str(volumeOfInsidePipe) + " m^3 = volume of inside pipe")
+print(str(volumeOfJustPipe) + " m^3 = volume of just pipe")
 
-crossSectionArea=math.pow(pipeDiameter,2) - math.pow(pipeDiameter-2*wallThickness,2)
-volumeOfPipe = crossSectionArea * pipeLength * math.pi/4000
+# mass = density of pipe * volume of pipe
+# mass in units of grams
+mass = steelDensity * volumeOfJustPipe
 
-mass = steelDensity * volumeOfPipe
-weightOfPipe = mass * 9.8
+# volume of water displaced is total volume enclosed by the pipe
+volumeOfWaterDisplaced = volumeOfOutsidePipe
 
 print(str(mass) + " kg = mass of pipe")
-print(str(volumeOfPipe) + " volume of pipe")
-print(str(weightOfPipe) + " N = Weight of pipe")
-
-# volume of a cylinder = pi * radius^2 * height
-volumeOfWaterDisplaced = math.pow((pipeDiameter / 2),2) * pipeLength * math.pi/4000
+print(str(volumeOfWaterDisplaced) + " m^3 = volume of water displaced")
 
 Fbuoyant = densityOfWater * 9.8 * volumeOfWaterDisplaced
+weightOfPipe = mass * 9.8
 
-print(str(Fbuoyant)+" N = Buoyant Force")
+print(str(weightOfPipe) + " N = Weight of pipe")
+print(str(Fbuoyant) + " N = Buoyant Force")
+
+if weightOfPipe > Fbuoyant:
+    print("The pipe will sink.")
+else:
+    print("The pipe will float.")
