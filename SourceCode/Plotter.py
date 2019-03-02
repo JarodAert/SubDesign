0dcdbbc17d841290fec878c4e663edd57626aa11
JarodAert <jarod.aerts@huskers.unl.edu>
2019-03-02 02:17:31 -0600

    kjdfd

--- SourceCode/Plotter.py
+++ SourceCode/Plotter.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import MaterialsFuncs
 
-DATA_FILE_PATH="./DataFiles/aluminumPipes.csv"
+DATA_FILE_PATH="./DataFiles/pvcPipes.csv"
 EXPORT_FILE_PATH="./DataFiles/boyancy.csv"
 GRAVITY=9.8
 WATER_DENSITY=1000
